e8bbb371e253cbd3349eaeecaec6615d9f410260
Ahmed Yousef <ahmedyousefx1@gmail.com>
2025-09-24 09:22:53 +0000

    Refactor settings and URLs to support dynamic ALLOWED_HOSTS and API root URL based on CODESPACE_NAME environment variable

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-p^42=dmdm-jypsr@ckvzt=pd)82z00+wb6#s-jil3nlma_hn-8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
